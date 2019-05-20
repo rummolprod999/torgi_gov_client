9dac6b1a88d2017bdb31869f684ff178802320c3
rummolprod999 <rummolprod999@gmail.com>
2019-05-20 12:00:14 +0300

    update

--- TgBot.js
+++ TgBot.js
@@ -1,17 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api');
-const channelId = "";
-const token ="";
+const channelId = "@@@@@@";
+const token ="@@@@@@@@@@@";
 
 class TgBot{
 
     constructor(message) {
-        this.bot = new TelegramBot(token, {polling: true});
+        this.bot = new TelegramBot(token, {polling: false});
         this.mes = message;
     }
 
     sendMessage(){
-        this.bot.sendMessage(channelId, this.mes).then(null, function (err) {
+        this.bot.sendMessage(channelId, this.mes, {parse_mode: "HTML"}).then(null, function (err) {
             throw err;
         });
     }
 }
+module.exports = TgBot;
